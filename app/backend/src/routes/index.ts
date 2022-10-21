a8b45171f30751440ed3267cf19dc12e6e608b8f
Caio Pereira Antunes <caioefg@gmail.com>
2022-10-21 18:00:50 -0300

    testando

---
+++ app/backend/src/routes/index.ts
@@ -0,0 +1,10 @@
+// import 'express-async-errors';
+// import { Application } from 'express';
+//
+// // import { loginRouter } from './category.routes';
+// // import { usersRouter } from './users.routes';
+//
+// export default (app: Application) => {
+//   app.use(usersRouter);
+//   // app.use(categoriesRouter);
+// };

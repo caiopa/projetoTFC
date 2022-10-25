f18591ab3a713027889db84a1379b1dc3b2c8ead
Caio Pereira Antunes <caioefg@gmail.com>
2022-10-24 21:12:22 -0300

    Testes rota login

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -16,18 +16,18 @@ const { expect } = chai;
 
 describe('Teste rota login ', () => {
   describe('Verificar se e possivel logar', async () => {
-    beforeEach(async () => {
+    async () => {
       sinon.stub(User, 'findOne').resolves(findUser as User);
 
       const resposta = await chai.request(app).post('/login').send({
         email: 'user@user.com',
         password: 'secret_user'
       });
-    })
-
-    afterEach(() => {
+    }
+    beforeEach(() => {
       sinon.restore();
     });
+    
     it('Verifica se retorna um status 400 quando email nao informado', async () => {
       const res  = await chai.request(app).post('/login').send({ password: 'invalid'})
 
@@ -39,6 +39,18 @@ describe('Teste rota login ', () => {
       expect(res.status).to.be.eq(400)
       expect(res.body).to.deep.equal({ message:  'All fields must be filled'})  
     });
+
+    it('Verifica se retora erro 401 quando o password for incorreto', async () => {
+      const res = await chai.request(app).post('/login').send({ email: 'user@user.com', password: 'invalid'})
+
+      expect(res.status).to.be.eq(401)
+      expect(res.body).to.deep.equal({ message: 'Incorrect email or password'})
+    });
+    it('Verifica se retora erro 401 quando o email for incorreto', async () => {
+      const res = await chai.request(app).post('/login').send({ email: 'invalid@user.com',  password: 'secret_user'})
+      expect(res.status).to.be.eq(401)
+      expect(res.body).to.deep.equal({ message: 'Incorrect email or password'})
+})
   })
  
   
